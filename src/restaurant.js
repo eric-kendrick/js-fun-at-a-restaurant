be22592e2404a33c8ff9862a08c378b42fe74d3e
Eric Kendrick <eric.c.kendrick@gmail.com>
2023-08-26 10:12:47 -0500

    Add function to create new resturant object, add menu item to restaurant menus, and remove menu items

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,8 +1,58 @@
+function createRestaurant(name) {
+  var restaurant = {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    },
+  };
+  return restaurant;
+}
 
+function addMenuItem(restaurant, menuItem) {
+  if (
+    menuItem.type === "breakfast" &&
+    restaurant.menus.breakfast.includes(menuItem) === false
+  ) {
+    restaurant.menus.breakfast.push(menuItem);
+  } else if (
+    menuItem.type === "lunch" &&
+    restaurant.menus.lunch.includes(menuItem) === false
+  ) {
+    restaurant.menus.lunch.push(menuItem);
+  } else if (
+    menuItem.type === "dinner" &&
+    restaurant.menus.dinner.includes(menuItem) === false
+  ) {
+    restaurant.menus.dinner.push(menuItem);
+  }
+}
+
+
+function removeMenuItem(restaurant, menuItem) {
+  for (let i = 0; i < restaurant.menus.breakfast.length; i++) {
+    if (menuItem === restaurant.menus.breakfast[i].name) {
+      restaurant.menus.breakfast.splice(i, 1);
+      return `No one is eating our ${menuItem} - it has been removed from the breakfast menu!`;
+    }
+    if (menuItem === restaurant.menus.lunch[i].name) {
+      restaurant.menus.lunch.splice(i, 1);
+      return `No one is eating our ${menuItem} - it has been removed from the lunch menu!`;
+    }
+    if (menuItem === restaurant.menus.dinner[i].name) {
+      restaurant.menus.dinner.splice(i, 1);
+      return `No one is eating our ${menuItem} - it has been removed from the dinner menu!`;
+    }
+    // else if (!restaurant.menus.breakfast[i].contains(menuItem)) {
+    //   return `Sorry, we don't sell ${menuItem}, try adding a new recipe!`;
+    // }
+  }
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem,
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem,
   // checkForFood
-}
+};
